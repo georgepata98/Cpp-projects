1bee0ab13181cd3885c81bf6f611d40946237c5e
George <122010570+georgepata98@users.noreply.github.com>
2026-06-03 19:30:50 +0300

    Update PLOT_functions.C

--- PLOT_functions.C
+++ PLOT_functions.C
@@ -1,32 +1,32 @@
-// Plotare Y(x) in ROOT
+// Fisierul cu date trebuie sa aiba 2 coloane: prima col. x si a doua Y
 {
-TGraph *gr1 = new TGraph();
 ifstream file;
-string filename;
+string fileName;
+
+cout << "\nNumele complet al fisierului text cu date (ex: data.txt):" << endl;
+cin >> fileName;
+
+file.open(fileName);
+if(!file)
+{
+    cout << "\nNu s-a putut deschide fisierul " << fileName << " ..." << endl;
+    exit(0);
+}
+
+TGraph *gr1 = new TGraph();
 double x, y;
 int i = 0;
-
-
-// Scrie mai jos numele fisierului de date (trebuie sa fie in acelasi folder cu acest cod):
-filename = "AmBe_TRS403.txt";
-// IMPORTANT: Fisierul cu date trebuie sa aiba 2 coloane (pe prima X si pe a doua Y).
-
-
-file.open(filename);
-while(1)
+while(!file.eof())
 {
     file >> x >> y;
-    if(file.eof()) break;
     gr1->SetPoint(i, x, y);
     i++;
 }
 file.close();
 
-TCanvas *c1 = new TCanvas("c1", "canvas", 1000, 600);
+TCanvas *c1 = new TCanvas("c1", "canvas", 800, 600);
 c1->SetGrid();
 gr1->SetLineWidth(1);
-gr1->SetLineColor(kBlue);
-gr1->SetMarkerColor(kBlue);
 gr1->SetMarkerStyle(20);
 gr1->GetXaxis()->SetTitle("x");
 gr1->GetYaxis()->SetTitle("Y(x)");
